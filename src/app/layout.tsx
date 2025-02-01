837f0f3110ca6d978159291ddda6d94d7a233355
Pietro <beelbonacossa@gmail.com>
2025-01-31 22:49:07 -0300

    mejoras en los metadatos

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,13 +3,44 @@ import { Inter } from "next/font/google"
 import "@/styles/globals.css"
 import Navbar from "@/components/layout/Navbar"
 import Footer from "@/components/layout/Footer"
-import { Phone } from "lucide-react" // Si usas un ícono de Lucide
 
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "Pueble S.A.",
-  description: "Tu socio confiable en maquinaria agrícola de alta calidad",
+  title: "Pueble S.A. | Maquinaria Agrícola de Alta Calidad",
+  description: "Tu socio confiable en maquinaria agrícola de alta calidad. Venta, servicio técnico y repuestos para equipos agrícolas.",
+  keywords: ["maquinaria agrícola", "agricultura", "equipos agrícolas", "Pueble SA", "implementos agrícolas", "repuestos agrícolas", "venta maquinaria", "servicio técnico agrícola"],
+  authors: [{ name: "Pueble S.A." }],
+  viewport: "width=device-width, initial-scale=1.0",
+  icons: {
+    icon: "/favicon.ico",
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
+  openGraph: {
+    type: "website",
+    locale: "es_ES",
+    url: "https://landing-page-pueble.vercel.app/",
+    title: "Pueble S.A. | Maquinaria Agrícola",
+    description: "Tu socio confiable en maquinaria agrícola de alta calidad. Venta, servicio técnico y repuestos para equipos agrícolas.",
+    images: [
+      {
+        url: "/pueblemeta.jpg", // Reemplaza con la ruta correcta de tu imagen
+        width: 1200,
+        height: 630,
+        alt: "Pueble S.A. Maquinaria Agrícola",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Pueble S.A. | Maquinaria Agrícola",
+    description: "Tu socio confiable en maquinaria agrícola de alta calidad. Venta, servicio técnico y repuestos para equipos agrícolas.",
+    images: ["/pueblemeta.jpg"], // Reemplaza con la ruta correcta de tu imagen
+    creator: "@pueblesa", // Si tienes cuenta de Twitter
+  },
 }
 
 export default function RootLayout({
@@ -25,8 +56,6 @@ export default function RootLayout({
           {children}
         </main>
         <Footer />
-
-        {/* Botón flotante WhatsApp */}
         <a
           href="https://web.whatsapp.com/send?phone=5493816618632&text=Hola%20estoy%20en%20la%20pagina%20web%20y%20quiero%20hacer%20una%20consulta!" 
           target="_blank"
