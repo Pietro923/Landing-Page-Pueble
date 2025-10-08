dec96c2f8a53bb9d4944b408d6d87cc84c868642
Pietro923 <beelbonacossa@gmail.com>
2025-10-08 10:20:18 -0300

    pequeño arreglo, horario mejor

--- src/components/layout/WhatsAppButton.tsx
+++ src/components/layout/WhatsAppButton.tsx
@@ -13,7 +13,7 @@ import {
     const options = [
       {
         title: "Servicio de Guardia de Servicios",
-        description: "Asistencia Lun-Vie 18:00-8:00",
+        description: "Asistencia 24/7",
         phone: "5493814901111",
         message: "Hola, necesito asistencia del servicio de guardia.",
         icon: <Phone className="w-5 h-5 text-green-600" />
